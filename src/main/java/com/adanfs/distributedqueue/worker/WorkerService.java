a5af1bfd49c2b86fb6e35c25946ef080bf3cab61
yesith <yesith@qbitum.com>
2024-05-24 21:07:08 +0100

    added monitoring

--- src/main/java/com/adanfs/distributedqueue/worker/WorkerService.java
+++ src/main/java/com/adanfs/distributedqueue/worker/WorkerService.java
@@ -1,6 +1,6 @@
 package com.adanfs.distributedqueue.worker;
 
 public interface WorkerService {
-    void receiveMessage(String message) throws InterruptedException;
-    void processMessageWithRetry(String message);
+//    void receiveMessage(String message) throws InterruptedException;
+//    void processMessageWithRetry(String message);
 }
